993f3f433e3f0049e8cf63b353c766ac2f39b2c3
ccloud8939 <cclo8939@byu.edu>
2025-03-27 23:48:10 -0600

    sdf

--- backend/Mission11_Cloud/Controllers/BooksController.cs
+++ backend/Mission11_Cloud/Controllers/BooksController.cs
@@ -5,50 +5,59 @@ namespace Mission11_Cloud.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-			    
     public class BooksController : ControllerBase
     {
-        private BooksDbContext _booksContext;
-	    
+        private readonly BooksDbContext _booksContext;
+
         public BooksController(BooksDbContext temp)
         {
             _booksContext = temp;
         }
+
         [HttpGet("AllBooks")]
-        public IActionResult GetBooks(int pageHowMany = 5, int pageNum = 1, string sortOrder = "asc")
+        public IActionResult GetBooks(int pageHowMany = 5, int pageNum = 1, string sortOrder = "asc", [FromQuery] List<string>? Category = null)
         {
-            var booksQuery = _booksContext.Books.AsQueryable();
+            var query = _booksContext.Books.AsQueryable();
+
             // Sorting Logic
             if (sortOrder.ToLower() == "asc")
             {
-                booksQuery = booksQuery.OrderBy(b => b.Title);
+                query = query.OrderBy(b => b.Title);
             }
             else
             {
-                booksQuery = booksQuery.OrderByDescending(b => b.Title);
+                query = query.OrderByDescending(b => b.Title);
             }
-            
-            var something = booksQuery
-                .Skip((pageNum - 1) * pageHowMany)
-                .Take(pageHowMany).ToList();
-            
-            var totalNumBooks = _booksContext.Books.Count();
 
-            var result = new
+            // Filtering by book types
+            if (Category != null && Category.Any())
             {
-                Books = something,
-                totalNumBooks = totalNumBooks
-            };
-            return Ok(result);
-        }
+                query = query.Where(b => Category.Contains(b.Category));
+            }
+
+            var totalNumBooks = _booksContext.Books.Count();
+            
+            var paginatedBooks = query
+                .Skip((pageNum - 1) * pageHowMany)
+                .Take(pageHowMany)
+                .ToList();
+
+            return Ok(new
+            {
+                Books = paginatedBooks,
+                TotalNumBooks = totalNumBooks
+            });
         }
 
-        //[HttpGet("FunctionalProjects")]
-        //public IEnumerable<Project> GetFunctionalProjects()
-        //{
-            //var something = _waterContext.Projects.Where(p => p.ProjectFunctionalityStatus == "Functional").ToList();
-            //return something;
-        //}
+        [HttpGet("GetBooksTypes")]
+        public IActionResult GetBooksTypes()
+        {
+            var query = _booksContext.Books
+                .Select(b => b.Category)
+                .Distinct()
+                .ToList();
+
+            return Ok(query);
+        }
     }
-    
-
+}
