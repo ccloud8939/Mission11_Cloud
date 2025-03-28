9dbb3ca04d5f3e2aa8fd898dc94a618069c09c14
ccloud8939 <cclo8939@byu.edu>
2025-03-28 14:24:46 -0600

    f

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,10 @@
-import { useState } from 'react';
 import './App.css'
-import BooksList from './BooksList'
-import CategoryFilter from './CategoryFilter'
-import WelcomeBand from './WelcomeBand'
+import BooksPage from './pages/BooksPage'
 
 function App() {
-  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
   return (
     <>
-      <div className="container mt-4">
-        <div className="row bg-primary text-white">
-          <WelcomeBand />
-        </div>
-        <div className="row">
-          <div className="col-md-3">
-            <CategoryFilter selectedCategories={selectedCategories} setSelectedCategories={setSelectedCategories}/>
-          </div>
-          <div className="col-md-9">
-            <BooksList selectedCategories = {selectedCategories} />
-          </div>
-        </div>
-    </div>
+      <BooksPage />
     </>
   )
 }
---
+++ frontend/src/pages/BooksPage.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import BooksList from "../BooksList";
+import CategoryFilter from "../CategoryFilter";
+import WelcomeBand from "../WelcomeBand";
+
+function BooksPage(){
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+    return(
+        <div className="container mt-4">
+                <div className="row bg-primary text-white">
+                  <WelcomeBand />
+                </div>
+                <div className="row">
+                  <div className="col-md-3">
+                    <CategoryFilter selectedCategories={selectedCategories} setSelectedCategories={setSelectedCategories}/>
+                  </div>
+                  <div className="col-md-9">
+                    <BooksList selectedCategories = {selectedCategories} />
+                  </div>
+                </div>
+            </div>
+    )
+}
+
+export default BooksPage;
